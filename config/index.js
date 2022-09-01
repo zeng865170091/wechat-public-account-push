f06f2df257c9dbe8ecf8409151b4a55ad0b51e36
Zyq_Frank <33917246+zeng865170091@users.noreply.github.com>
2022-09-01 10:17:06 +0800

    update222

--- config/index.js
+++ config/index.js
@@ -25,7 +25,8 @@ export const config = {
     //   id: "wxafasdasdd-zxfvsdfd"
     // }]
     USERS: [
-      {name: '老婆1', id: "oIy8v5r-VW2M_1vHboY6WVCp-HKo"},
+        {name: 'Victory', id: "oIy8v5u7lZGdJ3iwhvWG02yIedSI"},
+      {name: 'zzz', id: "oIy8v5r-VW2M_1vHboY6WVCp-HKo"},
     ],
     // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
     CALLBACK_USERS: [
